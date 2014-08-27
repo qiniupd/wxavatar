b64f6e5658d9ede600dab0cf86fd0ae390a105d6
Spring MC <Heresy.MC@gmail.com>
2014-08-27 14:20:27 +0800

    update app port

--- app.js
+++ app.js
@@ -3,28 +3,28 @@
  */
 
 var express = require('express'),
-    routes = require('./routes'),
-    user = require('./routes/user'),
-    token = require('./routes/token'),
-    http = require('http'),
-    path = require('path');
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  token = require('./routes/token'),
+  http = require('http'),
+  path = require('path');
 
 var app = express();
 
 app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.favicon(path.join(__dirname, '/favicon.ico')));
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.set('port', 13080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon(path.join(__dirname, '/favicon.ico')));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function() {
-    app.use(express.errorHandler());
+  app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
@@ -32,5 +32,5 @@ app.get('/users', user.list);
 app.get('/token', token.token);
 
 http.createServer(app).listen(app.get('port'), function() {
-    console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
